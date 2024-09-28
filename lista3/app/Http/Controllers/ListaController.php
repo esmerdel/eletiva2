74a2a2cdb3a8ed8047c1a343959172bb18f80845
esmerdel <leandro_esmerdel@hotmail.com>
2024-09-28 09:41:51 -0300

    Lista 3 - Exerc 1 e 2

---
+++ lista3/app/Http/Controllers/ListaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListaController extends Controller
+{
+    public function mostrarExer1(){
+        return view("exer1");
+    }
+
+    public function calcularExer1(Request $request){
+        $valor1 = (int)$request->input('valor1');
+        $valor2 = (int)$request->input('valor2');
+        return $valor1+$valor2;
+    }
+    public function mostrarEx02()
+    {
+        return view("exer2");
+    }
+    public function calcularEx02(Request $request)
+    {
+        $valor1 = (int)$request->input('val01');
+        $valor2 = (int)$request->input('val02');
+        return $valor1 - $valor2;
+    }
+}
---
+++ lista3/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListaController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/exer1', [ListaController::class, 'mostrarExer1']);
+Route::post('/respostaexer1', [ListaController::class, 'calcularExer1']);
+
+Route::get('/exer2', [ListaController::class, 'mostrarEx02']);
+Route::post('/respostaexer2', [ListaController::class, 'calcularEx02']);
+
